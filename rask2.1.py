294b6495305cf6b1162cad592de98d9d4967d78f
Petrolya <bairit777@gmail.com>
2024-02-21 16:01:14 +0300

    task2.1

--- rask2.1.py
+++ rask2.1.py
@@ -1,7 +1,5 @@
-print('Введите пароль!')
-pas = input()
-print('Подтвердите пароль!')
-pas2 = input()
+pas = input('Введите пароль!')
+pas2 = input('Подтвердите пароль!')
 if pas == pas2:
     print('Пароль принят!')
 else:
